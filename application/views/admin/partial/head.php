a4f49ca428a490e880036977cf7ed45547fc6611
ridiyoga <44762924+ridiyoga@users.noreply.github.com>
2019-10-22 01:00:32 +0700

    sidebar mari bro

--- application/views/admin/partial/head.php
+++ application/views/admin/partial/head.php
@@ -1,6 +1,6 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Merry's Collecti</title>
+    <title>Merry's Collection</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
