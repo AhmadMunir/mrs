d3198e58d19519d65ee2d4efefc0d7e688832d57
youwantorefo <44155450+youwantorefo@users.noreply.github.com>
2019-10-31 19:17:09 +0700

    session
    session

--- application/controllers/admin/Home.php
+++ application/controllers/admin/Home.php
@@ -2,6 +2,15 @@
   class Home extends CI_Controller{
     public function __construct(){
       parent::__construct();
+      if($this->session->userdata('status') != "login"){
+            //
+            redirect(base_url("login"));
+        }else{
+             if($this->session->userdata('tabel_admin') != "username"){
+                echo"anda bukan admin";
+                redirect(base_url("login"));
+            }
+        }
     }
 
     public function index(){
